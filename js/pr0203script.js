a51764c249062c6942ecbb6b326912343d62b2d9
IvxnG <gonzalez.martino.ivan@gmail.com>
2022-09-27 09:02:26 +0200

    practicas ut2 en proceso

--- js/pr0203script.js
+++ js/pr0203script.js
@@ -2,3 +2,10 @@ let kgManzanas = prompt('Manzanas');
 let kgNaranjasnas = prompt('Naranjas');
 let kgPlatano = prompt('Platanos');
 
+let $kg = $kgManzanas + $kgNaranjasnas + $kgPlatano;
+
+let $precio = ($kgManzanas*2) + ($kgNaranjasnas*1.6) +($kgPlatano*0.9)
+
+let $datos = "Has comprado un total de kilos de fruta y te ha costado <importe_total> euros"
+
+alert (datos);
